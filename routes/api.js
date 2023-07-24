a011358abd5da9261abe477dbac51360865f68d1
subhanmaulana1981 <subhanmaulana1981@gmail.com>
2023-07-24 10:16:43 +0700

    senin, 24 juli di method put revisi console log

--- routes/api.js
+++ routes/api.js
@@ -63,7 +63,7 @@ router.put("/ninjas/:id", function(req, res, next) {
         Ninja.findOne({_id: req.params.id})
         .then(function (ninja) {
             res.send(ninja);
-            console.log(ninjas);
+            console.log(ninja);
         })
     })
     .catch(next);
