c98aaf3eb68ae888bd7fd0ee9027d264a8821f54
subhanmaulana1981 <subhanmaulana1981@gmail.com>
2023-07-26 15:27:22 +0700

    rabu, 26 juli 23 sql statement like

--- routes/api.js
+++ routes/api.js
@@ -32,7 +32,7 @@ router.get("/ninjas/:id", function(req, res, next) {
 
 // get a ninja sounds like
 router.get("ninjas/name/:name", function(req, res, next) {
-    Ninja.find({name: req.params.name})
+    Ninja.find({name: req.params})
     .then(function (ninja) {
         res.send(ninja);
         console.log(ninja);
