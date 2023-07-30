7e355491a59b343f43291c578a42f1286b77b121
subhanmaulana1981 <subhanmaulana1981@gmail.com>
2023-07-30 09:07:29 +0700

    minggu, 30 juli efisiensi koding

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,6 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const router = express.Router();
 const Ninja = require("../models/ninjas");
-const MongoQS = require("mongo-querystring");
-const { query } = require("express");
-const { json } = require("body-parser");
-const qs = require("qs");
 
 // get a list of ninjas from the db
 router.get("/ninjas", function(req, res, next) {
@@ -42,27 +38,12 @@ router.get("/ninja", function(req, res, next) {
 
 // add a new ninja to the db
 router.post("/ninja", function(req, res, next) {
-    // a long way
-    /* var ninja = new Ninja(req.body);
-    ninja.save().then(function (ninja) {
-        res.send(ninja);
-        console.log(ninja);
-    }); */
-    
     // simple way
     Ninja.create(req.body)
     .then(function (ninja) {
         res.send(ninja);
         console.log(ninja);
     }).catch(next);
-
-    /* console.log("permintaan dengan metode POST dari klien");
-    console.log(req.body);
-    res.send({
-        type: "POST",
-        name: req.body.name,
-        rank: req.body.rank
-    }); */
 });
 
 // update a ninja in the db
@@ -76,21 +57,10 @@ router.put("/ninja/:id", function(req, res, next) {
         })
     })
     .catch(next);
-
-    /* console.log("permintaan dengan metode PUT dari klien");
-    res.send({
-        type: "PUT"
-    }); */
 });
 
 // delete a ninja from the db
 router.delete("/ninja/:id", function(req, res, next) {
-    /* console.log(req.params.id);
-    console.log("permintaan dengan metode DELETE dari klien");
-    res.send({
-        type: "DELETE"
-    }); */
-
     Ninja.findByIdAndRemove({_id: req.params.id})
     .then(function (ninja) {
         res.send(ninja);
